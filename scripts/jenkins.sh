5241fe332e0807d210336c5b8693bc6ca655ca1a
Ítalo Moura <italo.moura@escolaconquer.com.br>
2022-11-06 19:02:10 -0300

    jenkins

--- scripts/jenkins.sh
+++ scripts/jenkins.sh
@@ -1,2 +1,1 @@
-echo "Email $JENKINS_EMAIL"
-echo "Mandando e-mail com o mail do linux" | mail -s "subject: qualquer coisa" $JENKINS_EMAIL
+echo "Mandando e-mail com o mail do linux" | mail -s "subject: qualquer coisa" italo.moura1201@outlook.com
